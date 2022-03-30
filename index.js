8ba5206d8ea6b8444f67998344dfb664d6ca3f4d
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-03-30 23:43:34 +0400

    Added a way to start and pause the timer

--- index.js
+++ index.js
@@ -9,10 +9,23 @@ class Timer {
 
         //add event listener for startButton every time the button is clicked
         this.startButton.addEventListener('click', this.start)
+        this.pauseButton.addEventListener('click', this.pause)
     }
 
-    //create methods to start the timer
+    //create methods to start and pause the timer
     start() {
         console.log("Timer starts now")
     }
+
+    pause() {
+        console.log("Timer is now paused")
+    }
 }
+
+//create variables for each of the elements
+const durationInput = document.getElementById('timer-count')
+const startButton = document.getElementById('start')
+const pauseButton = document.getElementById('pause')
+
+//create a new instance of the timer
+const timer = new Timer(durationInput, startButton, pauseButton)
