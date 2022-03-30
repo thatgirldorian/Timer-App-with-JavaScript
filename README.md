b26f59041a8f7c75f3a0a6daccb90ae2a47d463e
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-03-30 23:04:41 +0400

    Added boilerplate content

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Timer-App-with-JavaScript
+
+This is a simple timer application that allows you to set a timer. It works like a stop watch and can count down from a number of minutes.
---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log("Hey, this works!")
+
+//Use an event listener to watch for a click on the start button
+// onStartButtonClick = function onClick 
+
+//Use a class and invoke our methods
+function onStartButtonClick (){
+    return "Hey there"
+}
+
+onStartButtonClick = () => {
+    return "Hey, that'a an arrow function"
+}
